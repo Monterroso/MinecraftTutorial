80b9ab523b67a61859d74dec7ce7356ceb320b3b
Marcus Monterroso <mmonterroso@berkeley.edu>
2022-01-08 14:50:22 -0800

    Added two items to the game

---
+++ src/main/java/net/lordoflizardz/onemillionbananas/item/ModItem.java
@@ -0,0 +1,24 @@
+package net.lordoflizardz.onemillionbananas.item;
+
+import net.lordoflizardz.onemillionbananas.OneMillionBananas;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItem {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, OneMillionBananas.MOD_ID);
+
+    public static final RegistryObject<Item> COBALT_INGOT = ITEMS.register("cobalt_ingot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> COBALT_NUGGET = ITEMS.register("cobalt_nugget",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
