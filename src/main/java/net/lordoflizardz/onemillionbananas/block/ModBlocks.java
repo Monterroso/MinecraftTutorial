5e79082f3c6f1fa8da3f53aabccdf0e45d10ac2d
Marcus Monterroso <mmonterroso@berkeley.edu>
2022-06-28 14:08:36 -0700

    Added custom block model

--- src/main/java/net/lordoflizardz/onemillionbananas/block/ModBlocks.java
+++ src/main/java/net/lordoflizardz/onemillionbananas/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.lordoflizardz.onemillionbananas.block;
 
 import net.lordoflizardz.onemillionbananas.OneMillionBananas;
+import net.lordoflizardz.onemillionbananas.block.custom.CobaltBlasterBlock;
 import net.lordoflizardz.onemillionbananas.block.custom.CobaltLampBlock;
 import net.lordoflizardz.onemillionbananas.block.custom.SpeedyBlock;
 import net.lordoflizardz.onemillionbananas.block.custom.TurnipCropBlock;
@@ -89,6 +90,9 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> POTTED_PINK_ROSE = BLOCKS.register("potted_pink_rose",
             () -> new FlowerPotBlock(null, ModBlocks.PINK_ROSE, BlockBehaviour.Properties.copy(Blocks.POTTED_DANDELION)));
+
+    public static final RegistryObject<Block> COBALT_BLASTER = registerBlock("cobalt_blaster",
+            () -> new CobaltBlasterBlock(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()), ModCreativeModeTab.COURSE_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab, String tooltipKey) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
