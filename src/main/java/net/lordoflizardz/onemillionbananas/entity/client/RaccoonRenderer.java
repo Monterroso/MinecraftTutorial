b5909b6aad9bfa681759a571a2ef0a4eb254dbec
Marcus Monterroso <mmonterroso@berkeley.edu>
2022-08-02 12:33:05 -0700

    Added custom entity variant

--- src/main/java/net/lordoflizardz/onemillionbananas/entity/client/RaccoonRenderer.java
+++ src/main/java/net/lordoflizardz/onemillionbananas/entity/client/RaccoonRenderer.java
@@ -1,9 +1,12 @@
 package net.lordoflizardz.onemillionbananas.entity.client;
 
+import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.lordoflizardz.onemillionbananas.OneMillionBananas;
 import net.lordoflizardz.onemillionbananas.entity.custom.RaccoonEntity;
+import net.lordoflizardz.onemillionbananas.entity.variant.RaccoonVariant;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -12,7 +15,18 @@ import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
+import java.util.Map;
+
 public class RaccoonRenderer extends GeoEntityRenderer<RaccoonEntity> {
+    public static final Map<RaccoonVariant, ResourceLocation> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(RaccoonVariant.class), (p_114874_) -> {
+                p_114874_.put(RaccoonVariant.DEFAULT,
+                        new ResourceLocation(OneMillionBananas.MOD_ID, "textures/entity/raccoon/raccoon.png"));
+                p_114874_.put(RaccoonVariant.DARK,
+                        new ResourceLocation(OneMillionBananas.MOD_ID, "textures/entity/raccoon/raccoondark.png"));
+                p_114874_.put(RaccoonVariant.RED,
+                        new ResourceLocation(OneMillionBananas.MOD_ID, "textures/entity/raccoon/redraccoon.png"));
+            });
 
     public RaccoonRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new RaccoonModel());
@@ -21,7 +35,7 @@ public class RaccoonRenderer extends GeoEntityRenderer<RaccoonEntity> {
 
     @Override
     public ResourceLocation getTextureLocation(RaccoonEntity instance) {
-        return new ResourceLocation(OneMillionBananas.MOD_ID, "textures/entity/raccoon/raccoon.png");
+        return LOCATION_BY_VARIANT.get(instance.getVariant());
     }
 
     @Override
