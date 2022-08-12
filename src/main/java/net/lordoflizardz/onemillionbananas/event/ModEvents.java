6da154f9d4c19708bb8affc7cc4a7496bf76a931
Marcus Monterroso <mmonterroso@berkeley.edu>
2022-08-11 19:14:55 -0700

    Added custom villager trades

--- src/main/java/net/lordoflizardz/onemillionbananas/event/ModEvents.java
+++ src/main/java/net/lordoflizardz/onemillionbananas/event/ModEvents.java
@@ -1,22 +1,31 @@
 package net.lordoflizardz.onemillionbananas.event;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.lordoflizardz.onemillionbananas.OneMillionBananas;
 import net.lordoflizardz.onemillionbananas.command.ReturnHomeCommand;
 import net.lordoflizardz.onemillionbananas.command.SetHomeCommand;
 import net.lordoflizardz.onemillionbananas.config.OneMillionBananasClientConfigs;
+import net.lordoflizardz.onemillionbananas.item.ModItem;
 import net.lordoflizardz.onemillionbananas.util.KaupenTitleScreen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.command.ConfigCommand;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = OneMillionBananas.MOD_ID)
 public class ModEvents {
@@ -55,6 +64,29 @@ public class ModEvents {
         if (OneMillionBananasClientConfigs.CUSTOM_TITLE_SCREEN.get() &&
                 event.getScreen() instanceof TitleScreen && !(event.getScreen() instanceof KaupenTitleScreen)) {
             event.setScreen(new KaupenTitleScreen());
+        }
+    }
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event) {
+        if(event.getType() == VillagerProfession.FARMER) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItem.TURNIP.get(), 12);
+            int villagerLevel = 1;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 2),
+                    stack,10,3,0.02F));
+        }
+
+        if(event.getType() == VillagerProfession.TOOLSMITH) {
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+            ItemStack stack = new ItemStack(ModItem.COBALT_PAXEL.get(), 1);
+            int villagerLevel = 3;
+
+            trades.get(villagerLevel).add((trader, rand) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 12),
+                    stack,4,5,0.09F));
         }
     }
 }
